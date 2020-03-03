6c18ad595401b3c5de2669cad6ab7343628789e0
daria.cappey <daria.cappey@outdoorative.com>
2020-03-03 16:48:49 +0100

    second question working

--- logic.js
+++ logic.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
+
   $(".background_1 .button_next").click(function(){
     $(".background_2").css("display","flex");
     $(".background_1").slideUp("slow");
     $('.background_1 [class^="question_"]').fadeOut("slow");
+    $(".button_next").css("display","none");
+    $('input[type="radio"]:checked').prop("checked",false);
   });
+
   $(".button_check").click(function(){
-    ans = document.querySelector('input[name="rad"]:checked').value;
+    ans = document.querySelector('input[type="radio"]:checked').value;
     console.log(ans);
     if (ans == "richtig") {
-      $('input[type="radio"]:checked + label img').css("border-color","green");
       $(".button_next").css("display","block");
     } else {
       $('input[type="radio"]:checked + label img').css("border-color","red");
