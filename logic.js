bd4d9ec3d3cedca8d07863e9ed3539b42e4f7b1d
daria.cappey <daria.cappey@outdoorative.com>
2020-03-04 09:24:05 +0100

    second question

--- logic.js
+++ logic.js
@@ -8,16 +8,39 @@ $(document).ready(function(){
     $('input[type="radio"]:checked').prop("checked",false);
   });
 
-  $(".button_check").click(function(){
+  $(".background_1 .button_check").click(function(){
     ans = document.querySelector('input[type="radio"]:checked').value;
     console.log(ans);
+    
     if (ans == "richtig") {
+      $('input[type="radio"]:checked + label img').css("border-color","green");
       $(".button_next").css("display","block");
     } else {
       $('input[type="radio"]:checked + label img').css("border-color","red");
-      setTimeout(function(){
-    $('input[type="radio"]:checked + label img').css("border-color","transparent");
-      }, 2000);
+    }
+  });
+
+  $(".background_2 .button_check").click(function(){
+    ans = document.getElementsByClassName("q2_inputs");
+    console.log(ans);
+    var right = true;
+    var wrong = [];
+    Array.from(ans).forEach(checkValue);
+
+    function checkValue(item) {
+      if (item.value != item.name){
+        right = false;
+        wrong.push(item);
+      }
+    }
+
+    if (right){
+      $('input[type="text"]').css("border-color","green");
+      $(".button_next").css("display","block");
+    } else {
+      wrong.forEach(function(item){
+        item.style["border-color"] = "red";
+      })
     }
   });
 });
